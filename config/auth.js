3c2a205b753a6d9a64f8fd768edcbccacf51e8ca
Muhammad Junaid Raza <48101492+Junaidraza75@users.noreply.github.com>
2021-01-21 19:24:25 +0500

    File2

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+    facebookAuth: {
+      clientID: '1022063214939077',
+      clientSecret: 'd72cd1073984a6dd8ec0ffe5722877f7',
+      callbackURL: 'http://localhost:5000/product/get',
+    },
+   
+    googleAuth: {
+      clientID: '262575533252-sviar9bm39g5c4dnlb6erkpvjme4hit2.apps.googleusercontent.com',
+      clientSecret: '1K1fRgOmy1WJK4_Z8ycGRyuz',
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+    },
+  };
---
+++ config/aws.js
@@ -0,0 +1,16 @@
+
+// const AWS = require("aws-sdk");
+// var multerS3 = require("multer-s3");
+
+// const ID = "AKIAIIA3W7AYKSQWJF4A";
+// const SECRET = "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";
+
+// // The name of the bucket that you have created
+// const BUCKET_NAME = "grocerydone4u";
+
+// const s3 = new AWS.S3({
+//   accessKeyId: ID,
+//   secretAccessKey: SECRET,
+// });
+
+// module.exports ={s3,BUCKET_NAME,multerS3}
